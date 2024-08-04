128c5f5cafbf6db8d5cf19266dd5b1649303c212
Abdessattar <ghabi.1602@gmail.com>
2024-08-04 02:32:01 +0000

    Update

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Defines a hash_password function to return a hashed password
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+            password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks whether a password is valid
+    Args:
+            hashed_password (bytes): hashed password
+            password (str): password in string
+    Return: boolean """
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
