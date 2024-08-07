e907953e2e4237b88049fb2cc8d592370ed08e10
Abdessattar <ghabi.1602@gmail.com>
2024-08-07 16:52:03 +0000

    Update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,17 +7,16 @@ from typing import (
         TypeVar
         )
 
+
 class Auth:
     """class Auth definition"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth func definition"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """authorization_header func definition"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user func definition"""
