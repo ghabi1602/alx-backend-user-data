f19eea7df5840791eba54b57f4bf0a1e9161d29c
Abdessattar <ghabi.1602@gmail.com>
2024-08-07 18:23:04 +0000

    Update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,22 @@ class Auth:
     """class Auth definition"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth func definition"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header func definition"""
