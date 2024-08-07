e17981aa88ddbba05c899ff3581f97d38df9108b
Abdessattar <ghabi.1602@gmail.com>
2024-08-07 16:45:10 +0000

    Update

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""a module that defines the Auth class"""
+
+from flask import request
+
+
+class Auth:
+    """class Auth definition"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth func definition"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header func definition"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user func definition"""
+        return None
