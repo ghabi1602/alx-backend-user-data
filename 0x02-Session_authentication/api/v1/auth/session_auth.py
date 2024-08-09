41a6813745f44abe3c733cf3324fae9f070b33a3
Abdessattar <ghabi.1602@gmail.com>
2024-08-09 17:02:56 +0000

    Update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """a module that defines a new authentication mechanism"""
 from .auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """a new class that manages session authentication"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            a method that creates a session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        sid = uuid.uuid4()
+        user_id_by_session_id[user_id] = sid
+        return sid
