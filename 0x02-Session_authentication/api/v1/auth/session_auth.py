0caecb8e5bc8c1fdd195e7520e32c885686d4c76
Abdessattar <ghabi.1602@gmail.com>
2024-08-09 18:00:28 +0000

    Update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -27,5 +27,5 @@ class SessionAuth(Auth):
         """
         if session_id is None or not instance(session_id, str):
             return None
-        
-        return self.user_id_by_session_id.get('session_id')
+
+        return str(self.user_id_by_session_id.get('session_id'))
