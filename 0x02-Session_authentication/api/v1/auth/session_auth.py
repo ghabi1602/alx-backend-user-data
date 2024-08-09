f3db1427e2aa0228180e3fe415b976e42f22318e
Abdessattar <ghabi.1602@gmail.com>
2024-08-09 16:19:10 +0000

    Update

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a module that defines a new authentication mechanism"""
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    """a new class that manages session authentication"""
