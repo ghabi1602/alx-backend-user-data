8947698d8585e5996dd335b4e8b3470411bca540
Abdessattar <ghabi.1602@gmail.com>
2024-08-10 00:25:22 +0000

    Update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Route module for the API.
+Route module for the API!!
 """
 from os import getenv
 from api.v1.views import app_views
@@ -23,12 +23,19 @@ elif AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
 def bef_req():
     """
-    Filter each request before it's handled by the proper route"""
+    Filter each request before it's handled by the proper route!
+    """
     if auth is None:
         pass
     else:
@@ -36,10 +43,12 @@ def bef_req():
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
@@ -54,14 +63,15 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Request unauthorized handler!!
+    """ Request unauthorized handler!!!
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Request unauthorized handler!!"""
+    """ Request unauthorized handler!!!
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
